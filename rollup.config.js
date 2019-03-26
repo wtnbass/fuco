0b706b5b91d0eccd14cfac92437efe39725ec5d9
wtnbass <bass.ksk@gmail.com>
2019-03-26 22:37:31 +0900

    Add Settings(build and lint)

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,45 @@
+import pkg from "./package.json";
 import resolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "dist/index.js",
-  output: {
-    file: "dist/bundle.js",
-    format: "esm"
-  },
-  plugins: [resolve()]
+const source = "dist/index.js";
+
+const terserOption = {
+  warnings: true
 };
+
+export default [
+  {
+    input: source,
+    output: {
+      file: pkg.main,
+      format: "cjs"
+    },
+    external: ["lit-html"]
+  },
+  {
+    input: source,
+    output: {
+      file: pkg["main:min"],
+      format: "cjs"
+    },
+    external: ["lit-html"],
+    plugins: [terser(terserOption)]
+  },
+  {
+    input: source,
+    output: {
+      file: pkg.module,
+      format: "esm"
+    },
+    plugins: [resolve()]
+  },
+  {
+    input: source,
+    output: {
+      file: pkg["module:min"],
+      format: "esm"
+    },
+    plugins: [resolve(), terser(terserOption)]
+  }
+];
