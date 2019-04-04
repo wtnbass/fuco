239065722a94567952e7d84e194bd04b1f061437
wtnbass <bass.ksk@gmail.com>
2019-04-04 22:08:03 +0900

    Update demo

--- demo/context-demo/theme2-consumer.ts
+++ demo/context-demo/theme2-consumer.ts
@@ -3,9 +3,11 @@ import { Theme2Context } from "./theme2-context";
 
 function Consumer() {
   const theme = useContext(Theme2Context);
+  const theme2 = useContext(Theme2Context);
+  const theme3 = useContext(Theme2Context);
   return html`
     <div>
-      Theme2 is ${theme}.
+      Theme2 is ${theme + theme2 + theme3}.
     </div>
   `;
 }
