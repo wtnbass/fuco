0ca1c2ab5ba015b4d2a95eb9f63aa149233c56b0
wtnbass <bass.ksk@gmail.com>
2019-04-07 22:22:50 +0900

    remove tested features from demo

--- demo/context-demo/theme2-context.ts
+++
@@ -1,7 +0,0 @@
-import { createContext, defineElement } from "../lib";
-
-export type Theme2 = "green" | "red";
-
-export const Theme2Context = createContext<Theme2>();
-
-defineElement("theme2-context", Theme2Context.Provider);
