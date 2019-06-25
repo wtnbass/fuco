8dbb4d3c3214af3c9407023cb3003914e87e6d5b
wtnbass <bass.ksk@gmail.com>
2019-06-25 22:15:28 +0900

    Add `useRef` instead of `useSelector`

--- src/hooks.ts
+++ src/hooks.ts
@@ -17,134 +17,160 @@ export function useAttribute<T>(
   name: string,
   converter?: AttributeConverter<T>
 ) {
-  return hooks<string | T | null>((h, c, i) => {
-    const m = new MutationObserver(() => {
-      const newValue = converter
-        ? converter(c.getAttribute(name))
-        : c.getAttribute(name);
-      if (h.values[i] !== newValue) {
-        h.values[i] = newValue;
-        c.update();
-      }
-    });
-    m.observe(c, { attributes: true, attributeFilter: [name] });
-    h.cleanup[i] = () => m.disconnect();
-    return converter ? converter(c.getAttribute(name)) : c.getAttribute(name);
-  });
-}
-
-export const useProperty = <T>(name: string) =>
-  hooks<T>((h, c, i) => {
-    const initialValue = (c as Component & { [name: string]: T })[name];
-
-    Object.defineProperty(c, name, {
-      get() {
-        return h.values[i];
-      },
-      set(newValue) {
+  return hooks<string | T | null>({
+    oncreate(h, c, i) {
+      const m = new MutationObserver(() => {
+        const newValue = converter
+          ? converter(c.getAttribute(name))
+          : c.getAttribute(name);
         if (h.values[i] !== newValue) {
           h.values[i] = newValue;
           c.update();
         }
-      }
-    });
-    return initialValue;
+      });
+      m.observe(c, { attributes: true, attributeFilter: [name] });
+      h.cleanup[i] = () => m.disconnect();
+      return converter ? converter(c.getAttribute(name)) : c.getAttribute(name);
+    }
+  });
+}
+
+export const useProperty = <T>(name: string) =>
+  hooks<T>({
+    oncreate(h, c, i) {
+      const initialValue = (c as Component & { [name: string]: T })[name];
+
+      Object.defineProperty(c, name, {
+        get() {
+          return h.values[i];
+        },
+        set(newValue) {
+          if (h.values[i] !== newValue) {
+            h.values[i] = newValue;
+            c.update();
+          }
+        }
+      });
+      return initialValue;
+    }
   });
 
-export const useSelector = <T extends Element>(selector: string) =>
-  hooks<{
-    readonly current: T | null;
-    readonly all: NodeListOf<T> | null;
-  }>((_, c) => ({
-    get current() {
-      return c.rootElement.querySelector<T>(selector);
-    },
-    get all() {
-      return c.rootElement.querySelectorAll<T>(selector);
-    }
-  }));
-
 export const useDispatchEvent = <T>(name: string, init: CustomEventInit = {}) =>
-  hooks<(detail: T) => void>((_, c) => (detail: T) =>
-    c.dispatchEvent(
-      new CustomEvent(name, {
-        ...init,
-        detail
-      })
-    )
-  );
+  hooks<(detail: T) => void>({
+    oncreate: (_, c) => (detail: T) =>
+      c.dispatchEvent(
+        new CustomEvent(name, {
+          ...init,
+          detail
+        })
+      )
+  });
 
 const enabledAdoptedStyleSheets =
   "adoptedStyleSheets" in Document.prototype &&
   "replace" in CSSStyleSheet.prototype;
 
 export const useStyle = (...styles: HasCSSSymbol[]) =>
-  hooks((h, c, i) => {
-    if (enabledAdoptedStyleSheets) {
-      for (let i = 0, l = styles.length; i < l; i++) {
-        const styleSheet = new CSSStyleSheet();
-        styleSheet.replace(styles[i][cssSymbol]);
-        c.rootElement.adoptedStyleSheets = [
-          ...c.rootElement.adoptedStyleSheets,
-          styleSheet
-        ];
-      }
-    } else {
-      h.effects[i] = () => {
+  hooks<undefined>({
+    oncreate(h, c, i) {
+      if (enabledAdoptedStyleSheets) {
         for (let i = 0, l = styles.length; i < l; i++) {
-          const style = document.createElement("style");
-          style.textContent = styles[i][cssSymbol];
-          c.rootElement.appendChild(style);
+          const styleSheet = new CSSStyleSheet();
+          styleSheet.replace(styles[i][cssSymbol]);
+          c.rootElement.adoptedStyleSheets = [
+            ...c.rootElement.adoptedStyleSheets,
+            styleSheet
+          ];
+        }
+      } else {
+        h.effects[i] = () => {
+          for (let i = 0, l = styles.length; i < l; i++) {
+            const style = document.createElement("style");
+            style.textContent = styles[i][cssSymbol];
+            c.rootElement.appendChild(style);
+          }
+        };
+      }
+      return undefined;
+    }
+  });
+
+const refPrefix = `ref:${String(Math.random()).slice(2)}:`;
+
+export const useRef = <T>(initialValue: T | null) =>
+  hooks<{ current: T | null }>({
+    oncreate: (_h, _c, i) =>
+      Object.defineProperties(Object.create(null), {
+        current: {
+          value: initialValue,
+          writable: true
+        },
+        toString: {
+          value: () => refPrefix + i
+        }
+      }),
+    onupdate: (h, c, i) => {
+      h.effects[i] = () => {
+        const value = c.rootElement.querySelector(`[ref="${refPrefix + i}"]`);
+        if (value != null) {
+          /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+          h.values[i].current = value as any;
         }
       };
+      return h.values[i];
     }
-    return 0;
   });
 
 export const useState = <T>(initialState: T) =>
-  hooks<[T, ((t: T | ((s: T) => T)) => void)]>((h, c, i) => [
-    initialState,
-    function setState(nextState: T | ((s: T) => T)) {
-      const state = h.values[i][0];
-      if (typeof nextState === "function") {
-        nextState = (nextState as (s: T) => T)(state);
+  hooks<[T, ((t: T | ((s: T) => T)) => void)]>({
+    oncreate: (h, c, i) => [
+      initialState,
+      function setState(nextState: T | ((s: T) => T)) {
+        const state = h.values[i][0];
+        if (typeof nextState === "function") {
+          nextState = (nextState as (s: T) => T)(state);
+        }
+        if (state !== nextState) {
+          h.values[i][0] = nextState;
+          c.update();
+        }
       }
-      if (state !== nextState) {
-        h.values[i][0] = nextState;
-        c.update();
-      }
-    }
-  ]);
+    ]
+  });
 
 export const useReducer = <S, A>(
   reducer: (state: S | undefined, action: A) => S,
   initialState: S
 ) =>
-  hooks<[S, (action: A) => void]>((h, c, i) => [
-    initialState,
-    function dispatch(action: A) {
-      const state = h.values[i][0];
-      const nextState = reducer(state, action);
-      if (state !== nextState) {
-        h.values[i][0] = nextState;
-        c.update();
+  hooks<[S, (action: A) => void]>({
+    oncreate: (h, c, i) => [
+      initialState,
+      function dispatch(action: A) {
+        const state = h.values[i][0];
+        const nextState = reducer(state, action);
+        if (state !== nextState) {
+          h.values[i][0] = nextState;
+          c.update();
+        }
       }
-    }
-  ]);
+    ]
+  });
 
 export const useContext = <T>(context: Context<T>) =>
-  hooks<T | undefined>((h, c, i) => {
-    if (!h.deps[i] || h.deps[i].length <= 0) {
-      dispatchCustomEvent(c, REQUEST_CONSUME, {
-        context,
-        consumer: c,
-        register(provider: Provider<T>) {
-          h.deps[i] = [provider];
-        }
-      });
+  hooks<T | undefined>({
+    onupdate: (h, c, i) => {
+      if (!h.deps[i] || h.deps[i].length <= 0) {
+        dispatchCustomEvent(c, REQUEST_CONSUME, {
+          context,
+          consumer: c,
+          register(provider: Provider<T>) {
+            h.deps[i] = [provider];
+          }
+        });
+      }
+      return (h.deps[i][0] as Provider<T>).value;
     }
-    return (h.deps[i][0] as Provider<T>).value;
-  }, true);
+  });
 
 const depsChanged = (prev: unknown[] | undefined, next: unknown[]) =>
   prev == null || next.some((f, i) => f !== prev[i]);
@@ -153,23 +179,27 @@ export const useEffect = (
   handler: () => void | (() => void),
   deps: unknown[] = []
 ) =>
-  hooks((h, _, i) => {
-    if (depsChanged(h.deps[i], deps)) {
-      h.deps[i] = deps;
-      h.effects[i] = handler;
+  hooks<undefined>({
+    onupdate(h, _, i) {
+      if (depsChanged(h.deps[i], deps)) {
+        h.deps[i] = deps;
+        h.effects[i] = handler;
+      }
+      return undefined;
     }
-    return 0;
-  }, true);
+  });
 
 export const useMemo = <T>(fn: () => T, deps: unknown[] = []) =>
-  hooks((h, _, i) => {
-    let value = h.values[i];
-    if (depsChanged(h.deps[i], deps)) {
-      h.deps[i] = deps;
-      value = fn();
+  hooks({
+    onupdate(h, _, i) {
+      let value = h.values[i];
+      if (depsChanged(h.deps[i], deps)) {
+        h.deps[i] = deps;
+        value = fn();
+      }
+      return value;
     }
-    return value;
-  }, true);
+  });
 
 export const useCallback = <A, R>(
   callback: (a: A) => R,
