912b5afa0294dcf487db71f829f2b14f3c7c71eb
wtnbass <bass.ksk@gmail.com>
2020-01-08 22:00:41 +0900

    refactor: make context more simple

--- src/core/context.ts
+++ src/core/context.ts
@@ -1,25 +1,69 @@
-import { Provider } from "./provider";
-
 export interface Context<T> {
   readonly initialValue?: T;
-  readonly id: number;
   readonly defineProvider: (name: string) => void;
 }
 
-let contextId = 0;
+export interface Detail<T> {
+  context: Context<T>;
+  register(provider: Provider<T>): void;
+}
+
+export const REQUEST_CONSUME =
+  "context:request-consume:" + String(Math.random()).slice(2);
+
+export abstract class Provider<T> extends HTMLElement {
+  protected abstract get context(): Context<T>;
+
+  private _value: T | undefined;
+  private listeners: (() => void)[] = [];
+
+  public constructor() {
+    super();
+    this.attachShadow({ mode: "open" }).appendChild(
+      document.createElement("slot")
+    );
+  }
+
+  protected connectedCallback() {
+    this.addEventListener(REQUEST_CONSUME, e => {
+      const { detail } = e as CustomEvent<Detail<T>>;
+
+      if (this.context === detail.context) {
+        e.stopPropagation();
+        detail.register(this);
+      }
+    });
+  }
+
+  public subscribe(subscriber: () => void) {
+    this.listeners.push(subscriber);
+    return () => {
+      const index = this.listeners.indexOf(subscriber);
+      this.listeners.splice(index, 1);
+    };
+  }
+
+  public get value() {
+    return this._value;
+  }
+
+  public set value(newValue) {
+    this._value = newValue;
+    this.listeners.forEach(l => l());
+  }
+}
 
 export function createContext<T>(initialValue?: T): Context<T> {
-  const id = contextId++;
-
   return {
     initialValue,
-    id,
     defineProvider(name: string) {
+      // eslint-disable-next-line @typescript-eslint/no-this-alias
+      const context = this;
       window.customElements.define(
         name,
         class extends Provider<T> {
-          protected get contextId() {
-            return id;
+          protected get context() {
+            return context;
           }
         }
       );
